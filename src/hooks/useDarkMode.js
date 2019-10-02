c82e9396b306ae4c7d359791c35a5daf852ebfd0
Anthony Hill <hillan1152@gmail.com>
2019-10-02 18:27:29 -0400

    completed hooks, now need to find out how to prevent default

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react';
+import { useLocalStorage } from './useLocalStorage';
+
+const useDarkMode = (key, initialValue) => {
+    const [darkMode, setDarkMode] = useLocalStorage(key, initialValue);
+    
+    // const handleChanges = key => {
+    //     setDarkMode(key);
+    // } 
+
+    useEffect((e) => {
+        
+        if(darkMode === true){
+            document.querySelector('body').classList.add('dark-mode');
+        } else {
+            document.querySelector('body').classList.remove('dark-mode')
+        }   
+    }, [darkMode])
+    return [darkMode, setDarkMode];
+}
+
+export default useDarkMode;
--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,6 +1,10 @@
 import { useState } from 'react';
 
 export const useLocalStorage = (key, initialValue) => {
+    if (typeof key !== 'string'){
+        // throw new Error ('Invalid Params')
+        console.log("string")
+    }
     const [storedValue, setStoredValue] = useState( () => {
         const item = window.localStorage.getItem(key);
         return item ? JSON.parse(item) : initialValue;
@@ -10,5 +14,5 @@ export const useLocalStorage = (key, initialValue) => {
         window.localStorage.setItem(key, JSON.stringify(value));
     }
 
-    return [storedValue, setValue];
+    return [storedValue, setStoredValue];
 }
