f7b5134accc708277543d93eb4432cdd8be116f2
Anthony Hill <hillan1152@gmail.com>
2019-10-30 17:32:58 -0400

    MVP

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,17 @@
+import { useEffect } from 'react';
+
+import { useLocalStorage } from './useLocalStorage';
+
+export const useDarkMode = (key, initialValue) => {
+    const [darkMode, setDarkMode] = useLocalStorage(key, initialValue);
+
+    useEffect(() => {
+        if (darkMode === true) {
+            document.querySelector('body').classList.add('dark-mode');
+        } else {
+            document.querySelector('body').classList.remove('dark-mode');
+        }
+    }, [darkMode])
+
+    return [darkMode, setDarkMode]
+}
